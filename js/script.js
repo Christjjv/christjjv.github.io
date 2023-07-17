c9976eb57da7ca83545e4aa17229bfd2f8d8ce3f
Christjjv <ferrarry7@hotmail.com>
2023-07-17 14:51:35 -0500

    v3

--- js/script.js
+++ js/script.js
@@ -4,28 +4,32 @@ window.onscroll = function(){
     /*var_header = document.getElementById("cabecera");*/
     var_header1 = document.getElementById("idUp");
     var_header2 = document.getElementById("idDown");
+    var_navega = document.getElementById("idUlNaveg");
 
     if(var_scroll > 20){
         /*var_header.classList.add('naveg-movi');*/
         var_header1.classList.add('naveg-movi1');
         var_header2.classList.add('naveg-movi2');
+        var_navega.classList.add('naveg-movi3');
     }else
     if(var_scroll < 20){
         /*var_header.classList.remove('naveg-movi');*/
         var_header1.classList.remove('naveg-movi1');
         var_header2.classList.remove('naveg-movi2');
+        var_navega.classList.remove('naveg-movi3');
     }
 }
 
 
 /*************** Para aparecer menu a lado derecho ***************/
 document.getElementById("boton-menu").addEventListener("click", mostrar_menu);
-var_menu = document.getElementById("cabecera");
+/*var_menu = document.getElementById("cabecera");*/
 /*var_texto = document.getElementById("idTexto");*/
-var_nav = document.getElementById("navegador");
+/*var_nav = document.getElementById("navegador");*/
+var_nav = document.getElementById("idUlNaveg");
 
 function mostrar_menu(){
-    var_menu.classList.toggle('move-content');
+    /*var_navega.classList.toggle('move-content');*/
     /*var_texto.classList.toggle('move-content');*/
     var_nav.classList.toggle('move-naveg');
 }
