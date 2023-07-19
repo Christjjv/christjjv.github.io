c807a6e37318249d0fbcefd5fabbff15eff4921b
Christjjv <ferrarry7@hotmail.com>
2023-07-19 17:36:34 -0500

    v5

--- js/script.js
+++ js/script.js
@@ -22,6 +22,7 @@ window.onscroll = function(){
 
 
 /*************** Para aparecer menu a lado derecho ***************/
+/*************** Para aparecer menu a lado izquierdo ***************/
 document.getElementById("boton-menu").addEventListener("click", mostrar_menu);
 /*var_menu = document.getElementById("cabecera");*/
 /*var_texto = document.getElementById("idTexto");*/
@@ -34,6 +35,96 @@ function mostrar_menu(){
     var_nav.classList.toggle('move-naveg');
 }
 
+
+
+
+/*************** Estido Slider - Carrusel **************/
+/*const carruselSlider = document.querySelector('.slider');
+const itemSlider = document.getElementsByClassName('slider-item');
+const itemPaginacion = document.getElementsByClassName('paginacion-item ');
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+
+let index = 1;
+
+width = itemSlider[0].clientWidth;
+carruselSlider.style.transform = 'translateX('+ (-width * index) + 'px)';
+
+function sliderWidth(){
+    width = itemSlider[0].clientWidth;
+}
+
+sliderWidth();
+window.addEventListener('resize', sliderWidth);
+
+//Go to next slide
+nextBtn.addEventListener('click', nextSlide);
+
+function nextSlide(){
+    if(index >= itemSlider.length - 1){return}
+    carruselSlider.style.transition = 'transform 0.4s ease-out';
+    index++;
+    carruselSlider.style.transform = 'translateX('+ (-width * index) + 'px)';
+
+    itemPaginacionLabel();
+}
+
+//Go to prev slide
+prevBtn.addEventListener('click', prevSlide);
+
+function prevSlide(){
+    if(index <= 0){return}
+    carruselSlider.style.transition = 'transform 0.4s ease-out';
+    index--;
+    carruselSlider.style.transform = 'translateX('+ (-width * index) + 'px)';
+
+    itemPaginacionLabel();
+}
+
+//return to the primera slider 
+carruselSlider.addEventListener('transitionend', function(){
+    if(itemSlider[index].id === 'firstImageDuplicate'){
+        carruselSlider.style.transition = 'none';
+        index = itemSlider.length - index;
+        carruselSlider.style.transform = 'translateX('+ (- width * index) + 'px)';
+
+        itemPaginacionLabel();
+    }
+
+    if(itemSlider[index].id === 'lastImageDuplicate'){
+        carruselSlider.style.transition = 'none';
+        index = itemSlider.length - 2;
+        carruselSlider.style.transform = 'translateX('+ (- width * index) + 'px)';
+
+        itemPaginacionLabel();
+    }
+});
+
+//auto sliding
+function autoSlider(){
+    deleteInterval = setInterval(timer, 3000);
+    function timer(){
+        nextSlide();
+    }
+}
+
+autoSlider();
+
+//stop auto sliding con el mouse
+const mainContainer = document.querySelector('.container-slider');
+mainContainer.addEventListener('mouseover', function(){
+    clearInterval(deleteInterval);
+});
+
+//resume sliding con el mouse
+mainContainer.addEventListener('mouseout', autoSlider);
+
+function itemPaginacionLabel(){
+    for(i = 0; i< itemPaginacion.length; i++){
+        itemPaginacion[i].className = itemPaginacion[i].className.replace(' active', '');
+    }
+    itemPaginacion[index - 1].className += ' active';
+}*/
 
 /******************* Messenger ***********************/
 /*
