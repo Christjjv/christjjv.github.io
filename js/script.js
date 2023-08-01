972e2e55d2053be5fec2203d437f17e1845aa722
Christjjv <ferrarry7@hotmail.com>
2023-08-01 14:49:11 -0500

    v0.5

--- js/script.js
+++ js/script.js
@@ -40,7 +40,9 @@ function mostrar_menu(){
 var chatbox = document.getElementById('fb-customer-chat');
 chatbox.setAttribute("page_id", "112327411899958");
 chatbox.setAttribute("attribution", "biz_inbox");
-chatbox.setAttribute("theme_color", "#2e3559");
+chatbox.setAttribute("theme_color", "#0A7CFF");
+chatbox.setAttribute("alignment", "left");
+//chatbox.setAttribute("margin-bottom", "10px");
 
 
 // Your SDK code 
