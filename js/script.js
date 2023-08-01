eb821e65cab215ebc449a2636988eb0425a074b4
Christjjv <ferrarry7@hotmail.com>
2023-08-01 13:12:51 -0500

    v0.3

--- js/script.js
+++ js/script.js
@@ -37,23 +37,24 @@ function mostrar_menu(){
 
 
 /******************* Messenger ***********************/
-/*
 var chatbox = document.getElementById('fb-customer-chat');
 chatbox.setAttribute("page_id", "112327411899958");
 chatbox.setAttribute("attribution", "biz_inbox");
 chatbox.setAttribute("theme_color", "#0084FF");
 
+
+// Your SDK code 
 window.fbAsyncInit = function() {
-    FB.init({
-        xfbml   : true,
-        version : 'v17.0'
-    });
+  FB.init({
+    xfbml            : true,
+    version          : 'v17.0'
+  });
 };
 
 (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));*/
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
